0b0083d8292211100cfa168a9644f5626e820d82
Ubuntu <ubuntu@ip-172-31-83-18.ec2.internal>
2024-10-14 13:39:22 +0000

    terra-config for docker cont

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "mycontainer" {
+  image = docker_image.nginx.image_id
+  name  = "mycontainer"
+}
